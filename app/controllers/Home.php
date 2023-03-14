b212b956d51469bdef00a39cf0d04aae8e7d8086
thuanbkk20 <thuan17082002@gmail.com>
2023-03-14 21:19:23 +0700

    Add function to render dashboard view

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -9,15 +9,37 @@ class Home extends Controller{
     }
 
     public function index(){
-        // Session::data('username','Minh Thuan');
-        // Session::data('email','thuan@gmail.com');
-        // Session::Flash('msg','Welcome');
-        // echo Session::Flash('msg');
-        // echo '<pre>';print_r(Session::data());echo '</pre>';
-
         $this->data['sub_content']['page_title'] = "Dashboard";
         // $this->data['sub_content']['product_list'] = $this->model_home->All();
         $this->data["content"] = 'dashboard/dashboard';
+        $this->render('layouts/basic_layout', $this->data);
+    }
+
+    public function soilHumid(){
+        $this->data['sub_content']['page_title'] = "Độ ẩm đất - Chi tiết";
+        // $this->data['sub_content']['product_list'] = $this->model_home->All();
+        $this->data["content"] = 'dashboard/soilHumid';
+        $this->render('layouts/basic_layout', $this->data);
+    }
+
+    public function airHumid(){
+        $this->data['sub_content']['page_title'] = "Độ ẩm không khí - Chi tiết";
+        // $this->data['sub_content']['product_list'] = $this->model_home->All();
+        $this->data["content"] = 'dashboard/airHumid';
+        $this->render('layouts/basic_layout', $this->data);
+    }
+
+    public function temperature(){
+        $this->data['sub_content']['page_title'] = "Nhiệt độ - Chi tiết";
+        // $this->data['sub_content']['product_list'] = $this->model_home->All();
+        $this->data["content"] = 'dashboard/temperature';
+        $this->render('layouts/basic_layout', $this->data);
+    }
+
+    public function light(){
+        $this->data['sub_content']['page_title'] = "Ánh sáng - Chi tiết";
+        // $this->data['sub_content']['product_list'] = $this->model_home->All();
+        $this->data["content"] = 'dashboard/light';
         $this->render('layouts/basic_layout', $this->data);
     }
 
