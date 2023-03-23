a1485b54576e23709bfa75f3b07c6b876c766d03
trongphu012345@gmail.com <75671199+trongphuhappy@users.noreply.github.com>
2023-03-23 11:02:13 +0700

    UI 0.1 - header, footer, 404

--- app/views/blocks/footer.php
+++ app/views/blocks/footer.php
@@ -1,2 +1,29 @@
 </hr>
-<h1>FOOTER</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IoT_Tomato</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <footer class="bg text-light py-3 position-fixed bottom-0 start-0 w-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>2023, thực hiện bởi nhóm TPPT</p>
+                    <p>Đông Hòa, Dĩ An, Bình Dương</p>
+                </div>
+                <div class="col-md-6 text-end">
+                    <p>Bản quyền © 2023</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+      
+</body>
+</html>
--- app/views/blocks/header.php
+++ app/views/blocks/header.php
@@ -1,2 +1,41 @@
-<h1>HEADER</h1>
-</hr>
+</hr><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IoT_Tomato</title>
+    <link rel="stylesheet" href="/public/assets/css/header.css">
+    <link rel="icon" type="image/x-icon" href="/public/assets/pics/tomato.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <header class="navbar navbar-expand-lg navbar-light bg">
+        <div class="card-header navbar-collapse">
+            <img class="logo" src="/public/assets/pics/tomato.png" alt="tomato">
+            <a class="navbar-brand " href="#">
+                <h1 class="container-fluid none_margin_padding"><strong class="name_1">TP</strong><strong class="name_2">PT</strong></h1>
+                <p class="none_margin_padding"><i class="sub1">IoT</i><i class="sub2"> to Tomato</i></p>
+            </a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="me-auto"></div>
+
+                <!-- <div class="set_icon"><a class="ti-settings" href="#"></a></div>
+                <div class="set_icon"><a class="ti-bell" href="#"></a></div> -->
+
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Tìm kiếm" aria-label="Search">
+                </form>
+
+                <div class="avatar">
+                    <img class="avatar_image" src="/public/assets/pics/avt.jpg" alt="Ảnh đại diện">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <script src="/main.js"></script>
+</body>
+</html>
