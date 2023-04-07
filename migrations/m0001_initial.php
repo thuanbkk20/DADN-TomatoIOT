63de165171655eaa04311fceb53d6cfc824b5ddf
thuanbkk20 <thuan17082002@gmail.com>
2023-04-07 23:35:03 +0700

    Update Data

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -5,82 +5,77 @@ class m0001_initial{
         $db = $dbObject->db;
         $sql = "CREATE DATABASE IF NOT EXISTS TomatoIoT;
         Use TomatoIoT;
-        
+       
         DROP TABLE IF EXISTS user;
         CREATE TABLE user(
-            id varchar(10) not null,
+            id INT PRIMARY KEY AUTO_INCREMENT,
             username varchar(50) not null,
             password varchar(100) not null,
             name varchar(50) not null,
             phone_number varchar(50) not null,
-            role varchar(50) not null,
-            primary key (id)
+            role varchar(50) not null
         );
-        
+       
         DROP TABLE IF EXISTS sensor;
         CREATE TABLE sensor(
-            id varchar(10) not null,
+            id INT PRIMARY KEY AUTO_INCREMENT,
             type varchar(50) not null,
-            last_sent_data date not null,
-            area varchar(10) not null,
-            primary key (id)
+            last_sent_data TIMESTAMP not null,
+            area INT not null
         );
-        
+       
         DROP TABLE IF EXISTS area;
         CREATE TABLE area(
-            id varchar(10) not null,
-            location varchar(50) not null,
-            name varchar(50) not null,
-            primary key(id)
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            location varchar(100) not null,
+            name varchar(50) not null
         );
-        
+       
         DROP TABLE IF EXISTS equipment;
         CREATE TABLE equipment(
-            id varchar(10) not null,
+            id INT PRIMARY KEY AUTO_INCREMENT,
             level int not null,
             type varchar(50) not null,
-            area varchar(10) not null,
-            primary key(id)
+		    name varchar(50) not null,
+            area INT not null
         );
-        
+       
         DROP TABLE IF EXISTS log;
         CREATE TABLE log(
-            id varchar(10) not null,
-            time date, 
-            description varchar(500) not null,
-            primary key(id)
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            time TIMESTAMP,
+            description varchar(500) not null
         );
-        
+
         DROP TABLE IF EXISTS setting;
         CREATE TABLE setting(
             type varchar(20) not null,
             max_value int not null,
             min_value int not null,
-            user_id varchar(10) not null,
-            time date not null,
+            user_id INT not null,
+            time TIMESTAMP not null,
             primary key(type)
         );
-        
+       
         DROP TABLE IF EXISTS env_index;
         CREATE TABLE env_index(
-            sensor_id varchar(10) not null,
-            time date not null,
-            value int not null,
-            primary key(sensor_id)
+            sensor_id INT PRIMARY KEY AUTO_INCREMENT,
+            time TIMESTAMP not null,
+            value int not null
         );
-        
+       
         ALTER TABLE env_index
         ADD FOREIGN KEY (sensor_id) REFERENCES sensor(id);
-        
+       
         ALTER TABLE setting
         ADD FOREIGN KEY (user_id) REFERENCES user(id);
-        
+       
         ALTER TABLE equipment
         ADD FOREIGN KEY (area) REFERENCES area(id);
-        
+       
         ALTER TABLE sensor
         ADD FOREIGN KEY (area) REFERENCES area(id);
-        
+       
         ALTER TABLE sensor
         ADD FOREIGN KEY (type) REFERENCES setting(type);";
 
--- migrations/m0002_importData.php
+++ migrations/m0002_importData.php
@@ -3,10 +3,27 @@ class m0002_importData{
     public function up(){
         $dbObject = new DB();
         $db = $dbObject->db;
+        $cur_time = date('Y-m-d H:i:s');
         //Insert data into user table
-        $sql = 'USE TomatoIoT;
-        INSERT INTO user
-        VALUES ("avfae13","admin@gmail.com","$2y$10$kR3IBZ4Aunux/zENSwsLremmklA5x.iJT4nXlRPlYCeGjeleRfJ4i","NguyenVanA","0123456789","admin");';
+        $sql = '
+        INSERT INTO TomatoIoT.user VALUES (1,"admin@gmail.com","$2y$10$kR3IBZ4Aunux/zENSwsLremmklA5x.iJT4nXlRPlYCeGjeleRfJ4i","NguyenVanA","0123456789","admin");
+        
+        INSERT INTO TomatoIoT.area VALUES (1,"0:0;10:0;10:10;0:10","Khu vuc 1");
+
+        INSERT INTO TomatoIoT.sensor VALUES (1,"temperature",$cur_time,1);
+        INSERT INTO TomatoIoT.sensor VALUES (2,"light",$cur_time,1);
+        INSERT INTO TomatoIoT.sensor VALUES (3,"air_humid",$cur_time,1);
+        INSERT INTO TomatoIoT.sensor VALUES (4,"soil_humid",$cur_time,1);
+        
+        
+        INSERT INTO TomatoIoT.equipment VALUES (1,0,"fan","Quạt máy 1",1);
+        INSERT INTO TomatoIoT.equipment VALUES (1,0,"pump","Máy bơm 1",1);
+        
+        
+        INSERT INTO TomatoIoT.setting VALUES ("temperature",27,21,1,$cur_time);
+        INSERT INTO TomatoIoT.setting VALUES ("light",3000,2000,1,$cur_time);
+        INSERT INTO TomatoIoT.setting VALUES ("air_humid",60,45,1,$cur_time);
+        INSERT INTO TomatoIoT.setting VALUES ("soil_humid",80,70,1,$cur_time);    ';
         $db->query($sql);
     }
 }
