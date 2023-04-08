f83ba1f198160aff9ecc77a189f08022bcae8388
thuanbkk20 <thuan17082002@gmail.com>
2023-04-08 23:27:51 +0700

    Update

--- core/AdafruitIO.php
+++ core/AdafruitIO.php
@@ -3,7 +3,7 @@ class AdaFruitIO {
 	public $key;
 	public $url;
 	
-	public function __construct($key='aio_kJce1278gviLyoWoub6u5W1hvvOt', $url="http://io.adafruit.com"){
+	public function __construct($key='aio_fkEF04vAj6xcvTtYyKEODFu0Dn28', $url="http://io.adafruit.com"){
 		$this->key = $key;
 		$this->url = $url;
 	}
