343518fb2bf23f0cc9a597a421a9adf76c93cf70
thuanbkk20 <thuan17082002@gmail.com>
2023-04-15 15:51:00 +0700

    Update

--- app/views/blocks/header.php
+++ app/views/blocks/header.php
@@ -116,7 +116,7 @@
 								<li><a class="dropdown-item" href="account.html">Account</a></li>
 								<li><a class="dropdown-item" href="settings.html">Settings</a></li>
 								<li><hr class="dropdown-divider"></li>
-								<li><a class="dropdown-item" href="login.html">Log Out</a></li>
+								<li><a class="dropdown-item" href=<?php echo _WEB_ROOT."/site/logout";?>>Log Out</a></li>
 							</ul>
 			            </div><!--//app-user-dropdown--> 
 		            </div><!--//app-utilities-->
