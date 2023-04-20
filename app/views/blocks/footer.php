4d0e65bb783660b099116b99ce19e5b88979fc4f
phii-1003 <phi.bui1003yurie@hcmut.edu.vn>
2023-04-20 23:39:08 +0700

    Edit basic_layout, header, footer, sidebar

--- app/views/blocks/footer.php
+++ app/views/blocks/footer.php
@@ -1,5 +1,5 @@
-<footer class="app-footer ">
-    <div class="container text-end py-3">
+<!--<footer class="app-footer ">-->
+<div class="container text-end py-3 bg-white rounded-top">
     <small class="copyright">Thực hiện bởi nhóm <strong style="color: forestgreen;">TPPT</strong> <span class="sr-only"></span> <strong>Đại Học Bách Khoa Thành Phố Hồ Chí Minh</strong> Bản quyền © 2023</small>
-    </div>
-</footer><!--//app-footer-->
+</div>
+<!--</footer><!--//app-footer-->-->
