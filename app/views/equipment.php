c7241eb0a25a39752efd2f8c57f9937ea3b44235
phii-1003 <phi.bui1003yurie@hcmut.edu.vn>
2023-04-23 03:18:58 +0700

    Add frontend phần tài khoản và thiết bị (chưa phân trang)

--- app/views/equipment.php
+++ app/views/equipment.php
@@ -1,7 +1,83 @@
 <div style="width:70%;float:right;">
     <?php 
-        echo 'Tất cả sensor đều thuộc khu vực 1, tình trạng kết nối đã có, mn tự thêm mô tả nếu muốn'; 
-        echo '<pre>'; print_r($sensor); echo '</pre>';
+        echo 'Tất cả sensor đều thuộc khu vực 1, tình trạng kết nối đã có, mn tự thêm mô tả nếu muốn';
+//        echo '<pre>'; print_r($sensor); echo '</pre>';
     ?>
 </div>
+
+<div class="app-wrapper">
+    <div class="app-content pt-3 p-md-3 p-lg-4">
+        <div class="container-xl">
+            <div class="row mb-4 justify-content-between">
+                <div class="col-auto">
+                    <h1 class="app-page-title">Nhiệt độ</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container-xl">
+            <div class="app-card app-card-orders-table shadow-sm mb-5">
+                <div class="app-card-body">
+                    <div class="table-responsive">
+                        <table class="table app-table-hover mb-0 text-start">
+                            <thead>
+                            <tr>
+                                <th class="cell col col-1"></th>
+                                <th class="cell col col-2">Khu vực</th>
+                                <th class="cell col col-4">Loại</th>
+                                <th class="cell col col-2">Tình trạng</th>
+                                <th class="cell col col-3">Mô tả</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                foreach ($sensor as $each):
+                            ?>
+                            <tr
+                                    style="<?php if ($each['type'] == 'Ánh sáng') {}
+                                ?>"
+                            >
+                                <td class="cell"><a role="button" class="btn-sm app-btn-secondary"
+                                                    data-bs-toggle="modal" data-bs-target="#viewModal">Xem</a></td>
+                                <td class="cell">Khu vực 1</td>
+                                <td class="cell">
+                                    <?php echo $each['type'] ?>
+                                </td>
+                                <td class="cell">
+	                                <?php echo $each['connect'] ?>
+                                </td>
+                                <td class="cell">
+                                    Thời gian gần nhất:
+	                                <?php echo $each['time'] ?>
+                                </td>
+                            </tr>
+                            <?php
+                                endforeach
+                            ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!--                    view-->
+                    <div class="modal fade" id="viewModal" tabindex="-1"
+                         aria-labelledby="exampleModalLabel" aria-hidden="true" aria-modal="true" role="dialog">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header text-center">
+                                    <h5 class="fw-bold">Ổn định</h5>
+                                </div>
+                                <div class="modal-body">
+                                    Kết nối của thiết bị ổn định
+                                    Lần cuối kết nối <a class="note">14 Oct</a> <a class="note">08:12 AM</a>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src=<?php echo _WEB_ROOT."/public/assets/js/equipment.js";?>></script>
--- app/views/user/list.php
+++ app/views/user/list.php
@@ -1,35 +1,102 @@
-<div style="width:70%;float:right;">
-<h2>Dữ liệu để mọi người dễ xem, chỉnh giao diện xong có thể xóa đoạn này</h2>
+<div class="app-wrapper">
+    <div class="app-content pt-3 p-md-3 p-lg-4">
+        <div class="container-xl">
+            <div class="row g-3 mb-4 align-items-center justify-content-between">
+                <div class="col-auto">
+                    <h1 class="app-page-title mb-0">Tài khoản</h1>
+                </div>
+                <div class="col-auto">
+                    <div class="page-utilities">
+                        <div class="row g-2 justify-content-start justify-content-md-end align-items-center">
+                            <div class="col-auto">
+                                <form class="table-search-form row gx-1 align-items-center">
+                                    <div class="col-auto">
+                                        <input type="text" id="search-orders" name="searchorders" class="form-control search-orders" placeholder="Search">
+                                    </div>
+                                    <div class="col-auto">
+                                        <button type="submit" class="btn bg-primary text-light mx-1">Search</button>
+                                    </div>
+                                    <div class="col-auto">
+                                        <a role="button" class="btn btn-outline-danger"
+                                                href=<?php echo _WEB_ROOT."/admin/UserModify/create"; ?>>
+                                            Thêm tài khoản
+                                        </a>
+                                    </div>
+                                </form>
+                            </div><!--//col-->
+                        </div><!--//row-->
+                    </div><!--//table-utilities-->
+                </div><!--//col-auto-->
+            </div><!--//row-->
+
+            <div class="tab-content" id="orders-table-tab-content">
+                <div class="tab-pane fade show active" id="orders-all" role="tabpanel" aria-labelledby="orders-all-tab">
+                    <div class="app-card app-card-orders-table shadow-sm mb-5">
+                        <div class="app-card-body">
+                            <div class="table-responsive">
+                                <table class="table app-table-hover mb-0 text-left">
+                                    <thead>
+                                    <tr>
+                                        <th class="cell col col-1">Tên</th>
+                                        <th class="cell col col-3">Họ và tên lót</th>
+                                        <th class="cell col col-2">Tên tài khoản</th>
+                                        <th class="cell col col-2">Số điện thoại</th>
+                                        <th class="cell col col-2">Vai trò</th>
+<!--                                        <th class="cell col col-2">Chức năng</th>-->
+                                        <th class="cell col col-2">Chức năng</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
 <?php
-    echo '<pre>'; print_r($userArr); echo '</pre>';
-?> 
+    foreach($userArr as $user):
+        ?>
+                                    <tr>
+                                        <td class="cell"><?php echo $user['first_name']; ?></td>
+                                        <td class="cell"><?php echo $user['last_name']; ?></td>
+                                        <td class="cell fw-bold"><?php echo $user['username']; ?></td>
+                                        <td class="cell"><?php echo $user['phone_number']; ?></td>
+                                        <td class="cell"><?php echo $user['role']; ?></td>
+                                        <td class="cell row justify-content-start">
+                                            <a class="col-3" role="button" href=<?php echo _WEB_ROOT."/admin/UserModify/update?id=".$user['id']; ?>><svg class="fa-solid fa-edit"></svg></a>
+                                            <a class="col-3" role="button"
+                                               data-bs-toggle="modal" data-bs-target="#confirm_modal"><svg class="fa-solid fa-trash-can"></svg></a>
+                                        </td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                        <div class="modal fade" id="confirm_modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-body text-center align-text-bottom" style="min-height: 150px;">
+                                                        Xoá tài khoản này?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Đóng</button>
+                                                        <button type="button" class="btn btn-success" href=<?php echo _WEB_ROOT."/admin/UserModify/delete?id=".$user['id']; ?>>Xác nhận</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </tbody>
+                                </table>
+                            </div><!--//table-responsive-->
+
+                        </div><!--//app-card-body-->
+                    </div><!--//app-card-->
+                    <nav class="app-pagination">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                            </li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav><!--//app-pagination-->
+                </div><!--//tab-pane-->
+            </div><!--//tab-content-->
+        </div><!--//container-fluid-->
+    </div><!--//app-content-->
 </div>
-<div style="width:70%;float:right;">
-    <a href=<?php echo _WEB_ROOT."/admin/UserModify/create"; ?>>Thêm tài khoản</a>
-</div>
-<table style="width:70%;float:right;">
-  <tr>
-    <th>Tên</th>
-    <th>Họ và tên lót</th>
-    <th>Tên tài khoản</th>
-    <th>Số điện thoại</th>
-    <th>Vai trò</th>
-    <th>Chứ năng</th>
-  </tr>
-  <?php
-    foreach($userArr as $user){
-        echo '<tr>';
-        echo '<td>'.$user['first_name'].'</td>';
-        echo '<td>'.$user['last_name'].'</td>';
-        echo '<td>'.$user['username'].'</td>';
-        echo '<td>'.$user['phone_number'].'</td>';
-        echo '<td>'.$user['role'].'</td>';
-        echo '<td>';
-        if($user['role'] =='Giám sát'){
-            echo "<a href="._WEB_ROOT."/admin/UserModify/update?id=".$user['id'].">Sửa</a>";
-            echo "<a href="._WEB_ROOT."/admin/UserModify/delete?id=".$user['id'].">Xóa</a>";
-        }
-        echo '</td></tr>';
-    }
-  ?>
-</table>
