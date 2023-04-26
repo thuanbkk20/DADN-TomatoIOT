a6b42c4801caf720ad21cfdf0a36e0ff5d67291f
thuanbkk20 <thuan17082002@gmail.com>
2023-04-27 01:16:05 +0700

    Js file to update log on header

--- public/assets/js/checkData.js
+++ public/assets/js/checkData.js
@@ -21,4 +21,51 @@ function f(){
             console.log('autoFan')
         }});
     }
+    //Kiểm tra tình trạng kết nối của các sensor
+    $.ajax({url:webRoot+'/Equipment/checkConnect',success: function(result){
+        console.log("Check Connection")
+    }});
+
+    //Thêm thông báo mới
+    $.ajax({url:webRoot+'/Home/autoAddLog',success: function(result){
+        $.ajax({url:webRoot+'/public/assets/json/log.json',success: function(result){
+            console.log("Add new log");
+            console.log(result);
+            let count = 0;
+            for(let item of result){
+                console.log("#time"+count)
+                $("#des"+item['id']).html(item['description'])
+                $("#time"+count).html(dateDiff(item['time']))
+                count++;
+            }
+        }});
+    }});
 }
+
+function dateDiff(date) {
+    var mydate = new Date();
+    var theDiff = "";
+    var datetime1 = new Date(date);
+    var datetime2 = new Date(mydate);
+    var diffInMilliSeconds = datetime2.getTime() - datetime1.getTime();
+    var seconds = Math.floor(diffInMilliSeconds / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+    var months = Math.floor(days / 30);
+    var years = Math.floor(months / 12);
+    if (years > 0) {
+      return years + " năm trước";
+    } else if (months > 0) {
+      return months + " tháng trước";
+    } else if (days > 0) {
+      return days + " ngày trước";
+    } else if (hours > 0) {
+      return hours + " giờ trước";
+    } else if (minutes > 0) {
+      return minutes + " phút trước";
+    } else {
+      return seconds + " giây trước";
+    }
+  }
+  
--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -5,10 +5,11 @@ $(document).ready(function () {
 function dashboard(){
     console.log('f() Dashboard')
     var webRoot = $("#webRoot").val()
+    //Lấy dữ liệu từ các cảm biến và in ra dashboard
     $.ajax({url:webRoot+'/public/assets/json/envIndex.json',success: function(result){
         console.log(result)
 
-        $("#soilHumidLastData").html(result['soilHumid']['LastData'])
+        $("#soilHumidLastData").html(result['soilHumid']['LastData']+"%")
         $("#soilHumidLastUpdate").html(result['soilHumid']['LastUpdate'])
         if(result['soilHumid']['LastData']>result['soilHumid']['maxValue']){
             $("#soilHumidLastUpdate").css("color", "red");
@@ -19,7 +20,7 @@ function dashboard(){
         }
 
 
-        $("#tempLastData").html(result['temperature']['LastData'])
+        $("#tempLastData").html(result['temperature']['LastData']+"℃")
         $("#tempLastUpdate").html(result['temperature']['LastUpdate'])
         if(result['temperature']['LastData']>result['temperature']['maxValue']){
             $("#tempLastData").css("color", "red");
@@ -29,7 +30,7 @@ function dashboard(){
             $("#tempLastData").css("color", "black");
         }
 
-        $("#lightLastData").html(result['light']['LastData'])
+        $("#lightLastData").html(result['light']['LastData']+"Lux")
         $("#lightLastUpdate").html(result['light']['LastUpdate'])
         if(result['light']['LastData']>result['light']['maxValue']){
             $("#lightLastData").css("color", "red");
@@ -39,7 +40,7 @@ function dashboard(){
             $("#lightLastData").css("color", "black");
         }
 
-        $("#airHumidLastData").html(result['airHumid']['LastData'])
+        $("#airHumidLastData").html(result['airHumid']['LastData']+"%")
         $("#airHumidLastUpdate").html(result['airHumid']['LastUpdate'])
         if(result['airHumid']['LastData']>result['airHumid']['maxValue']){
             $("#airHumidLastData").css("color", "red");
@@ -49,4 +50,5 @@ function dashboard(){
             $("#airHumidLastData").css("color", "black");
         }
     }});
+
 }
